f564cdbbd54b36bf8fe022beed4daa3261329a06
K-D-T <Kenyon.dwayne@gmail.com>
2022-12-15 12:55:19 -0600

    There should be a lot of info

--- mid/temp/week11/c-file.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void c_print(const char *s)
-{
-  printf("%s", s);
-}
--- mid/temp/week11/cpp-main.cpp
+++
@@ -1,8 +0,0 @@
-#include "c-file.h"
-
-int main(int argc, char *argv[])
-{
-  c_print("Hello World!\n");
-
-  return 0;
-}
